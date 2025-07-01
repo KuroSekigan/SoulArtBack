0931b7771e96adbbb853f0d450edd786c6a9a1b8
Luis Alfredo Carrillo Pasos <spidercraftnoir@gmail.com>
2025-06-30 21:05:50 -0600

    packages

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Importaciones
 import express from 'express';
-/* import mysql from 'mysql2';
 import cors from 'cors';
+/* import mysql from 'mysql2';
 import bcrypt from 'bcryptjs'; */
 
 // Inicialización
