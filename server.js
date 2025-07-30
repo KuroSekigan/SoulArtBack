13fa4cb73c295891a3267153d4d72dd7008a1094
Luis Alfredo Carrillo Pasos <spidercraftnoir@gmail.com>
2025-07-30 02:54:55 -0600

    fix

--- server.js
+++ server.js
@@ -839,7 +839,7 @@ app.delete('/favoritos', (req, res) => {
 });
 
 app.post('/comics/:comicId/reaccion', (req, res) => {
-    const token = req.headers.authorization;
+    const token = req.headers.authorization?.split(' ')[1];
     const { tipo } = req.body; // "like" o "dislike"
     const comicId = req.params.comicId;
 
