4a5b388e4890f7f3d75102ce7df24f21b40a76f2
TheRaxeor <eduardofarjattr@gmail.com>
2025-09-19 17:20:14 -0600

    Login con Google

---
+++ firebase.js
@@ -0,0 +1,9 @@
+import admin from "firebase-admin";
+import serviceAccount from "/serviceAccountKey.json" assert { type: "json" };
+
+// Inicializar Firebase Admin con la clave privada
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+export default admin;
