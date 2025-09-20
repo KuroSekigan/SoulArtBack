84969a5178e3941cd173c358a2c37e976a672d6f
TheRaxeor <eduardofarjattr@gmail.com>
2025-09-19 18:10:29 -0600

    a ver qpdito

--- firebase.js
+++
@@ -1,9 +0,0 @@
-import admin from "firebase-admin";
-import serviceAccount from "/serviceAccountKey.json" assert { type: "json" };
-
-// Inicializar Firebase Admin con la clave privada
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-});
-
-export default admin;
